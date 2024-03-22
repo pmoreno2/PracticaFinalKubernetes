f81e13c976ea7eb5b8fededf0aa974a8a82d2014
Pedro Moreno <pmoreno@iesemilidarder.com>
2024-03-22 23:09:31 +0100

    Exercise 2 improved. Added zip files to attach to the classroom task.

--- Exercise 2/Exercise2.sh
+++ Exercise 2/Exercise2.sh
@@ -1,23 +1,26 @@
-#Installation
+#STEP 1: Installation
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
-#Getting Nginx chart from Bitnami
-helm install Exercise2 oci://registry-1.docker.io/bitnamicharts/nginx
-#Revision and pods
+#STEP 2: Getting Nginx chart from Bitnami
+helm install exercise2 oci://registry-1.docker.io/bitnamicharts/nginx
+sleep 10
+#STEP 3: Revision and pods
 helm list
 kubectl get pods
-#Getting helm configuration file
+#STEP 4: Getting helm configuration file
 helm show values oci://registry-1.docker.io/bitnamicharts/nginx > values.yaml
-#Example of helm configuration file with Nginx and wrong version
- #-->values2.yaml
-#Updating chart from modified config file
-helm upgrade --install -f values.yaml Exercise2 oci://registry-1.docker.io/bitnamicharts/nginx
-#Wrong results
+#STEP 5: Example of helm configuration file with Nginx and wrong version
+ sed -i 's/tag: 1.25.4-debian-12-r3/tag: 1000000.25.4-debian-12-r3/g' values.yaml
+#STEP 6: Updating chart from modified config file
+helm upgrade --install -f values.yaml exercise2 oci://registry-1.docker.io/bitnamicharts/nginx
+sleep 10
+#STEP 7: Wrong results
 helm list
 kubectl get pods
-#Rollback to revision 1
-helm rollback Exercise2 1
-#Restablished results from revision 1
+#STEP 8: Rollback to revision 1
+helm rollback exercise2 1
+sleep 10
+#STEP 9: Restablished results from revision 1
 helm list
 kubectl get pods
